6d8c9220df9171601bdf1b2cefcf764222cc8f1a
benn <benn@DESKTOP-IMTHR3V>
2022-12-02 15:55:50 +0100

    Add project files.

---
+++ Base de donnees/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Base_de_donnees
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BtnAjouter = new System.Windows.Forms.Button();
+            this.BtnModifier = new System.Windows.Forms.Button();
+            this.BtnSupprimer = new System.Windows.Forms.Button();
+            this.BtnEnregistrer = new System.Windows.Forms.Button();
+            this.BtnAnnuler = new System.Windows.Forms.Button();
+            this.txtBoxNom = new System.Windows.Forms.TextBox();
+            this.txtBoxPrenom = new System.Windows.Forms.TextBox();
+            this.cbx = new System.Windows.Forms.ComboBox();
+            this.table2BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this._tp_c_DataSet = new Base_de_donnees._tp_c_DataSet();
+            this.table_2TableAdapter = new Base_de_donnees._tp_c_DataSetTableAdapters.Table_2TableAdapter();
+            this._tp_c_DataSet1 = new Base_de_donnees._tp_c_DataSet1();
+            this.table3BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.table_3TableAdapter = new Base_de_donnees._tp_c_DataSet1TableAdapters.Table_3TableAdapter();
+            this.txtBoxId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.table2BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._tp_c_DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._tp_c_DataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table3BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnAjouter
+            // 
+            this.BtnAjouter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BtnAjouter.Location = new System.Drawing.Point(58, 285);
+            this.BtnAjouter.Name = "BtnAjouter";
+            this.BtnAjouter.Size = new System.Drawing.Size(93, 35);
+            this.BtnAjouter.TabIndex = 0;
+            this.BtnAjouter.Text = "Ajouter";
+            this.BtnAjouter.UseVisualStyleBackColor = false;
+            this.BtnAjouter.Click += new System.EventHandler(this.BtnAjouter_Click);
+            // 
+            // BtnModifier
+            // 
+            this.BtnModifier.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.BtnModifier.Location = new System.Drawing.Point(157, 285);
+            this.BtnModifier.Name = "BtnModifier";
+            this.BtnModifier.Size = new System.Drawing.Size(93, 35);
+            this.BtnModifier.TabIndex = 1;
+            this.BtnModifier.Text = "Modifier";
+            this.BtnModifier.UseVisualStyleBackColor = false;
+            this.BtnModifier.Click += new System.EventHandler(this.BtnModifier_Click);
+            // 
+            // BtnSupprimer
+            // 
+            this.BtnSupprimer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.BtnSupprimer.Location = new System.Drawing.Point(258, 285);
+            this.BtnSupprimer.Name = "BtnSupprimer";
+            this.BtnSupprimer.Size = new System.Drawing.Size(93, 35);
+            this.BtnSupprimer.TabIndex = 2;
+            this.BtnSupprimer.Text = "Supprimer";
+            this.BtnSupprimer.UseVisualStyleBackColor = false;
+            this.BtnSupprimer.Click += new System.EventHandler(this.BtnSupprimer_Click);
+            // 
+            // BtnEnregistrer
+            // 
+            this.BtnEnregistrer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BtnEnregistrer.Location = new System.Drawing.Point(357, 285);
+            this.BtnEnregistrer.Name = "BtnEnregistrer";
+            this.BtnEnregistrer.Size = new System.Drawing.Size(93, 35);
+            this.BtnEnregistrer.TabIndex = 3;
+            this.BtnEnregistrer.Text = "Enregistrer";
+            this.BtnEnregistrer.UseVisualStyleBackColor = false;
+            this.BtnEnregistrer.Click += new System.EventHandler(this.BtnEnregistrer_Click);
+            // 
+            // BtnAnnuler
+            // 
+            this.BtnAnnuler.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BtnAnnuler.Location = new System.Drawing.Point(456, 285);
+            this.BtnAnnuler.Name = "BtnAnnuler";
+            this.BtnAnnuler.Size = new System.Drawing.Size(93, 35);
+            this.BtnAnnuler.TabIndex = 4;
+            this.BtnAnnuler.Text = "Annuler";
+            this.BtnAnnuler.UseVisualStyleBackColor = false;
+            // 
+            // txtBoxNom
+            // 
+            this.txtBoxNom.Location = new System.Drawing.Point(157, 100);
+            this.txtBoxNom.Name = "txtBoxNom";
+            this.txtBoxNom.Size = new System.Drawing.Size(128, 22);
+            this.txtBoxNom.TabIndex = 5;
+            // 
+            // txtBoxPrenom
+            // 
+            this.txtBoxPrenom.Location = new System.Drawing.Point(157, 143);
+            this.txtBoxPrenom.Name = "txtBoxPrenom";
+            this.txtBoxPrenom.Size = new System.Drawing.Size(128, 22);
+            this.txtBoxPrenom.TabIndex = 6;
+            // 
+            // cbx
+            // 
+            this.cbx.DataSource = this.table3BindingSource;
+            this.cbx.DisplayMember = "Prenom";
+            this.cbx.FormattingEnabled = true;
+            this.cbx.Location = new System.Drawing.Point(121, 43);
+            this.cbx.Name = "cbx";
+            this.cbx.Size = new System.Drawing.Size(251, 24);
+            this.cbx.TabIndex = 7;
+            this.cbx.ValueMember = "Prenom";
+            // 
+            // table2BindingSource
+            // 
+            this.table2BindingSource.DataMember = "Table_2";
+            this.table2BindingSource.DataSource = this._tp_c_DataSet;
+            // 
+            // _tp_c_DataSet
+            // 
+            this._tp_c_DataSet.DataSetName = "_tp_c_DataSet";
+            this._tp_c_DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // table_2TableAdapter
+            // 
+            this.table_2TableAdapter.ClearBeforeFill = true;
+            // 
+            // _tp_c_DataSet1
+            // 
+            this._tp_c_DataSet1.DataSetName = "_tp_c_DataSet1";
+            this._tp_c_DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // table3BindingSource
+            // 
+            this.table3BindingSource.DataMember = "Table_3";
+            this.table3BindingSource.DataSource = this._tp_c_DataSet1;
+            // 
+            // table_3TableAdapter
+            // 
+            this.table_3TableAdapter.ClearBeforeFill = true;
+            // 
+            // txtBoxId
+            // 
+            this.txtBoxId.Location = new System.Drawing.Point(157, 186);
+            this.txtBoxId.Name = "txtBoxId";
+            this.txtBoxId.Size = new System.Drawing.Size(128, 22);
+            this.txtBoxId.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nom : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Prenom :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "CIN : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxId);
+            this.Controls.Add(this.cbx);
+            this.Controls.Add(this.txtBoxPrenom);
+            this.Controls.Add(this.txtBoxNom);
+            this.Controls.Add(this.BtnAnnuler);
+            this.Controls.Add(this.BtnEnregistrer);
+            this.Controls.Add(this.BtnSupprimer);
+            this.Controls.Add(this.BtnModifier);
+            this.Controls.Add(this.BtnAjouter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.table2BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._tp_c_DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._tp_c_DataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table3BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnAjouter;
+        private System.Windows.Forms.Button BtnModifier;
+        private System.Windows.Forms.Button BtnSupprimer;
+        private System.Windows.Forms.Button BtnEnregistrer;
+        private System.Windows.Forms.Button BtnAnnuler;
+        private System.Windows.Forms.TextBox txtBoxNom;
+        private System.Windows.Forms.TextBox txtBoxPrenom;
+        private System.Windows.Forms.ComboBox cbx;
+        private _tp_c_DataSet _tp_c_DataSet;
+        private System.Windows.Forms.BindingSource table2BindingSource;
+        private _tp_c_DataSetTableAdapters.Table_2TableAdapter table_2TableAdapter;
+        private _tp_c_DataSet1 _tp_c_DataSet1;
+        private System.Windows.Forms.BindingSource table3BindingSource;
+        private _tp_c_DataSet1TableAdapters.Table_3TableAdapter table_3TableAdapter;
+        private System.Windows.Forms.TextBox txtBoxId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Base de donnees/Form1.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Base_de_donnees
+{
+    public partial class Form1 : Form
+    {
+        string last_click_on = "";
+        string Nom;
+        string Prenom;
+        int id; 
+
+        static string chaine = @"Data Source=DESKTOP-IMTHR3V\SQLEXPRESS;Initial Catalog=tp_c#;Integrated Security=True";
+        static SqlConnection cnx = new SqlConnection(chaine);
+        static SqlCommand cmd = new SqlCommand();
+        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void turn_On_Off(Button btn, bool b)
+        {
+            btn.Enabled = b;
+            if (b)
+            {
+                btn.BackColor = Color.White;
+
+
+            }
+            else
+            {
+                btn.BackColor = Color.Gray;
+            }
+        }
+
+        public bool tableHasRows()
+        {
+            cnx.Open();
+            cmd.Connection = cnx;
+            cmd.CommandText = "SELECT nom FROM Table_3;";
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                reader.Close();
+                cnx.Close();
+                return true;
+            }
+            else
+            {
+                reader.Close();
+                cnx.Close();
+                return true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the '_tp_c_DataSet1.Table_3' table. You can move, or remove it, as needed.
+            this.table_3TableAdapter.Fill(this._tp_c_DataSet1.Table_3);
+            
+
+            turn_On_Off(BtnAjouter, true);
+            turn_On_Off(BtnModifier, false);
+            turn_On_Off(BtnSupprimer, false);
+            turn_On_Off(BtnEnregistrer, false);
+            turn_On_Off(BtnAnnuler, false);
+
+            cnx.Open();
+            cmd.Connection = cnx;
+            cmd.CommandText = "select   nom,id  from Table_3";
+
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            DataRow itemrow = dt.NewRow();
+            itemrow[0] = "select name";
+            dt.Rows.InsertAt(itemrow, 0);
+
+            cbx.DataSource = dt;
+            cbx.ValueMember = "id";
+            cbx.DisplayMember = "nom ";
+
+            cnx.Close();
+
+            if (tableHasRows())
+            {
+                turn_On_Off(BtnModifier, true);
+                turn_On_Off(BtnSupprimer, true);
+            }
+
+        }
+
+            private void BtnAjouter_Click(object sender, EventArgs e)
+            {
+                last_click_on = "ajouter";
+                {
+                    cbx.Enabled = false;
+                    cbx.BackColor = Color.Gray;
+
+                    txtBoxNom.Enabled = true;
+                    txtBoxNom.BackColor = Color.White;
+
+                txtBoxId.Enabled = true;
+                txtBoxId.BackColor = Color.White;
+
+
+                txtBoxPrenom.Enabled = true;
+                    txtBoxPrenom.BackColor = Color.White;
+
+                    turn_On_Off(BtnAjouter, false);
+                    turn_On_Off(BtnModifier, false);
+                    turn_On_Off(BtnSupprimer, false);
+                    turn_On_Off(BtnEnregistrer, true);
+                    turn_On_Off(BtnAnnuler, true);
+                }
+            }
+            private void BtnModifier_Click(object sender, EventArgs e)
+            {
+                last_click_on = "modifier";
+                {
+                    cbx.Enabled = false;
+                    cbx.BackColor = Color.Gray;
+
+                    txtBoxNom.Enabled = true;
+                    txtBoxNom.BackColor = Color.White;
+
+                    txtBoxPrenom.Enabled = true;
+                    txtBoxPrenom.BackColor = Color.White;
+
+                txtBoxId.Enabled = true;
+                txtBoxId.BackColor = Color.White;
+
+                turn_On_Off(BtnAjouter, false);
+                    turn_On_Off(BtnModifier, false);
+                    turn_On_Off(BtnSupprimer, false);
+                    turn_On_Off(BtnEnregistrer, true);
+                    turn_On_Off(BtnAnnuler, true);
+                }
+            }
+            private void BtnSupprimer_Click(object sender, EventArgs e)
+            {
+                last_click_on = "supprimer";
+                {
+                    cbx.Enabled = true;
+                    cbx.BackColor = Color.White;
+
+                    txtBoxNom.Enabled = false;
+                    txtBoxNom.BackColor = Color.Gray;
+
+                    txtBoxPrenom.Enabled = false;
+                    txtBoxPrenom.BackColor = Color.Gray;
+
+                txtBoxId.Enabled = false;
+                txtBoxId.BackColor = Color.Gray;
+
+                turn_On_Off(BtnAjouter, false);
+                    turn_On_Off(BtnModifier, false);
+                    turn_On_Off(BtnSupprimer, false);
+                    turn_On_Off(BtnEnregistrer, true);
+                    turn_On_Off(BtnAnnuler, true);
+                }
+            }
+            private void BtnEnregistrer_Click(object sender, EventArgs e)
+            {
+                if (last_click_on == "ajouter")
+                {
+                    
+                    {
+                        cbx.Enabled = true;
+                        cbx.BackColor = Color.White;
+
+                        txtBoxNom.Enabled = false;
+                        txtBoxNom.BackColor = Color.Gray;
+
+                        txtBoxPrenom.Enabled = false;
+                        txtBoxPrenom.BackColor = Color.Gray;
+
+                    txtBoxId.Enabled = false;
+                    txtBoxId.BackColor = Color.Gray;
+
+                    turn_On_Off(BtnAjouter, true);
+                        turn_On_Off(BtnModifier, true);
+                        turn_On_Off(BtnSupprimer, true);
+                        turn_On_Off(BtnEnregistrer, false);
+                        turn_On_Off(BtnAnnuler, false);
+                    }
+                   
+                    {
+                        Nom = txtBoxNom.Text;
+                        Prenom = txtBoxPrenom.Text;
+                        if (Nom == "" || Prenom == "")
+                        {
+                            Console.WriteLine("invalid nom et prenom");
+                            return;
+                        }
+                        cnx.Open();
+                        cmd.Connection = cnx;
+                        cmd.CommandText = "insert into Table_3( prenom,nom,id ) values('" + Prenom + " ','" + Nom + "','"+txtBoxId.Text+"')";
+                        cmd.ExecuteNonQuery();
+                    cnx.Close();
+                        txtBoxNom.Text = "";
+                        txtBoxPrenom.Text = "";
+
+                        Nom = txtBoxNom.Text;
+                        Prenom = txtBoxPrenom.Text;
+                    }
+                }
+                else if (last_click_on == "supprimer")
+                {
+                  
+                    {
+                        cbx.Enabled = true;
+                        cbx.BackColor = Color.White;
+
+                        txtBoxNom.Enabled = false;
+                        txtBoxNom.BackColor = Color.Gray;
+
+                        txtBoxPrenom.Enabled = false;
+                        txtBoxPrenom.BackColor = Color.Gray;
+
+                        turn_On_Off(BtnAjouter, true);
+                        turn_On_Off(BtnModifier, false);
+                        turn_On_Off(BtnSupprimer, false);
+                        turn_On_Off(BtnEnregistrer, false);
+                        turn_On_Off(BtnAnnuler, false);
+                        if (tableHasRows())
+                        {
+                            turn_On_Off(BtnModifier, true);
+                            turn_On_Off(BtnSupprimer, true);
+                        }
+                    }
+                
+                if (MessageBox.Show("Est Vous Sur ?!", "Delete Record", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                        cnx.Open();
+                        cmd.Connection = cnx;
+                        string deleted_id = cbx.SelectedValue.ToString();
+                        Console.WriteLine(deleted_id);
+                        cmd.CommandText = "delete from Table_3 where id ='" + deleted_id + "' ";
+                        cmd.ExecuteNonQuery();
+                        cnx.Close();
+                        MessageBox.Show("deleted"); 
+                }
+                else
+                    MessageBox.Show("Merci !");
+                 }
+                else if (last_click_on == "modifier")
+                {
+
+                    
+                    {
+                        cbx.Enabled = true;
+                        cbx.BackColor = Color.White;
+
+                        txtBoxNom.Enabled = false;
+                        txtBoxNom.BackColor = Color.Gray;
+
+                        txtBoxPrenom.Enabled = false;
+                        txtBoxPrenom.BackColor = Color.Gray;
+
+                        turn_On_Off(BtnAjouter, true);
+                        turn_On_Off(BtnModifier, true);
+                        turn_On_Off(BtnSupprimer, true);
+                        turn_On_Off(BtnEnregistrer, false);
+                        turn_On_Off(BtnAnnuler, false);
+                    }
+                    
+                    {
+                        cnx.Open();
+                        cmd.Connection = cnx;
+                        string modified_id = cbx.SelectedValue.ToString();
+                        cmd.CommandText = " update Table_3 set nom ='" + txtBoxNom.Text + "', prenom = '" + txtBoxPrenom.Text + "' where id= '" + modified_id + "' ;";
+                        cmd.ExecuteNonQuery();
+                        cnx.Close();
+                    }
+                }
+
+                {
+                    cnx.Open();
+                    cmd.Connection = cnx;
+                    cmd.CommandText = "select   nom,id  from Table_3";
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    DataRow itemrow = dt.NewRow();
+                    itemrow[0] = "select name";
+                    dt.Rows.InsertAt(itemrow, 0);
+
+                    cbx.DataSource = dt;
+                    cbx.DisplayMember = "nom ";
+                    cbx.ValueMember = "id";
+                    cnx.Close();
+                }
+            }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void BtnAnnuler_Click(object sender, EventArgs e)
+        {
+            if (last_click_on == "ajouter")
+            {
+                {
+                    if (!tableHasRows())
+                    {
+                        turn_On_Off(BtnModifier, false);
+                        turn_On_Off(BtnSupprimer, false);
+                    }
+                    else
+                    {
+                        turn_On_Off(BtnModifier, true);
+                        turn_On_Off(BtnSupprimer, true);
+                    }
+
+
+                    cbx.Enabled = true;
+                    cbx.BackColor = Color.White;
+
+                    txtBoxNom.Enabled = false;
+                    txtBoxNom.BackColor = Color.Gray;
+
+                    txtBoxPrenom.Enabled = false;
+                    txtBoxPrenom.BackColor = Color.Gray;
+
+                    txtBoxId.Enabled = false;
+                    txtBoxId.BackColor = Color.Gray;
+
+                    turn_On_Off(BtnAjouter, true);
+                    turn_On_Off(BtnEnregistrer, false);
+                    turn_On_Off(BtnAnnuler, false);
+                }
+
+
+            }
+            else if (last_click_on == "supprimer")
+            {
+                {
+                    cbx.Enabled = true;
+                    cbx.BackColor = Color.White;
+
+                    txtBoxNom.Enabled = false;
+                    txtBoxNom.BackColor = Color.Gray;
+
+                    txtBoxPrenom.Enabled = false;
+                    txtBoxPrenom.BackColor = Color.Gray;
+
+                    txtBoxId.Enabled = false;
+                    txtBoxId.BackColor = Color.Gray;
+
+                    turn_On_Off(BtnAjouter, true);
+                    turn_On_Off(BtnModifier, true);
+                    turn_On_Off(BtnSupprimer, true);
+                    turn_On_Off(BtnEnregistrer, false);
+                    turn_On_Off(BtnAnnuler, false);
+                }
+
+            }
+            else if (last_click_on == "modifier")
+            {
+                {
+                    cbx.Enabled = true;
+                    cbx.BackColor = Color.White;
+
+                    txtBoxNom.Enabled = false;
+                    txtBoxNom.BackColor = Color.Gray;
+
+                    txtBoxPrenom.Enabled = false;
+                    txtBoxPrenom.BackColor = Color.Gray;
+
+                    txtBoxId.Enabled = false;
+                    txtBoxId.BackColor = Color.Gray;
+
+                    turn_On_Off(BtnAjouter, true);
+                    turn_On_Off(BtnModifier, true);
+                    turn_On_Off(BtnSupprimer, true);
+                    turn_On_Off(BtnEnregistrer, false);
+                    turn_On_Off(BtnAnnuler, false);
+                }
+
+            }
+
+  
+            {
+                cnx.Open();
+                cmd.Connection = cnx;
+                cmd.CommandText = "select   nom,id  from Etudiant";
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                DataRow itemrow = dt.NewRow();
+                itemrow[0] = "select name";
+                dt.Rows.InsertAt(itemrow, 0);
+
+                cbx.DataSource = dt;
+                cbx.ValueMember = "id";
+                cbx.DisplayMember = "nom ";
+                cnx.Close();
+            }
+        }
+
+    }
+}
+
